07e33e75dfc9a63f0d5b32f7a009d0976926b061
Evandro da Costa <evandrodacosta@gmail.com>
2022-03-03 10:16:54 -0300

    Add __str__ to Document class in reader/forms.py

--- reader/models.py
+++ reader/models.py
@@ -5,3 +5,6 @@ class Document(models.Model):
     filename = models.CharField(max_length=300)
     document = models.FileField()
     uploaded_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.filename} - {self.uploaded_at}'
